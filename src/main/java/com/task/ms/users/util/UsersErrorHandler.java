e4aec88dd8b460410e19f39a01c97493894a9aeb
Maria Eugenia Musatadi <mmusatad@everis.com>
2020-02-28 11:59:01 -0300

    Insercion comentarios a UsersErrorHandler

--- src/main/java/com/task/ms/users/util/UsersErrorHandler.java
+++ src/main/java/com/task/ms/users/util/UsersErrorHandler.java
@@ -8,9 +8,19 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.server.ResponseStatusException;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+
+/**
+ * Se controla que los mensajes de error que responde el Controller sean comprensibles por el usuario
+ * y tengasn el formato definido.
+ * Clase que intercepta las excepciones propagadas por el Controller
+ * @author mmusatad
+ *
+ */
 @ControllerAdvice
 public class UsersErrorHandler {
 
+	//Maneja excepción de negocio. Responde con el status de la excepción.
 	@ExceptionHandler({ ResponseStatusException.class })
 	public ResponseEntity<ErrorDto> handleUsersException(ResponseStatusException ex) {
 		
@@ -21,8 +31,10 @@ public class UsersErrorHandler {
 		return new ResponseEntity<ErrorDto>(err, new HttpHeaders(), ex.getStatus());
 	}
 	
-	@ExceptionHandler({ MethodArgumentNotValidException.class })
-	public ResponseEntity<ErrorDto> handleNotValidException(MethodArgumentNotValidException ex) {
+	//Maneja excepciones para el caso de atributos que no siguen el formato especificado
+	//según Pattern o Email.
+	@ExceptionHandler({ MethodArgumentNotValidException.class, InvalidFormatException.class })
+	public ResponseEntity<ErrorDto> handleNotValidException(Exception ex) {
 		
 		ErrorDto err = ErrorDto.builder()
 				.message(ex.getMessage())
